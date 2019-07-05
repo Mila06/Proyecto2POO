f853c1e73de70ac415b82ba428f74ebb67a7cca9
ST-19 <44902884+ST-19@users.noreply.github.com>
2019-07-05 16:11:02 -0500

    Update Objeto.cpp

--- Objeto.cpp
+++ Objeto.cpp
@@ -6,6 +6,10 @@ Objeto::Objeto(const TipoString& nombre, TipoCaracter color,
                TipoEntero posX, TipoEntero posY):
                                                  nombre{nombre}, color{color},
                                                  posX{posX}, posY{posY} {}
+Objeto::Objeto(const TipoString& nombre, TipoCaracter color,
+               TipoEntero posX, TipoEntero posY, double calificacion):
+                                                 nombre{nombre}, color{color},
+                                                 posX{posX}, posY{posY} , calificacion{calificacion}{}
 
 Objeto::~Objeto() {}
 
@@ -19,4 +23,19 @@ TipoCaracter Objeto::getColor()  { return color; }
 
 TipoString Objeto::mostrarPosicion() {
     return "X = " + to_string(posX) + " Y = " + to_string(posY);
+}
+
+restaurante::restaurante(const TipoString& nombre, TipoCaracter color,
+           TipoEntero posX, TipoEntero posY, double calificacion, string _tipoComida, string _especialidaD):objeto(nombre,color, posX,posY, calificacion){
+  tipoComida= _tipoComida;
+  especialidaD= _especialidaD;
+}
+hoteles::hoteles(const TipoString& nombre, TipoCaracter color,
+           TipoEntero posX, TipoEntero posY, double calificacion, double _estrellas, int _disponibilidad):objetos(nombre,color,posX,posY,calificacion){
+  disponibilidad= _disponibilidad;
+  estrellas= _estrellas;
+}
+museo::museo(const TipoString& nombre, TipoCaracter color,
+           TipoEntero posX, TipoEntero posY, double calificacion, string _exposicionA):objetos(nombre,color,posX,posY,calificacion){
+  exposicionA= _exposicionA;
 }
