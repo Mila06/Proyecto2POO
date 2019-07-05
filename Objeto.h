24d5f5beef89839f450090dd2ace291d1ed5187d
ST-19 <44902884+ST-19@users.noreply.github.com>
2019-07-05 15:50:34 -0500

    Update Objeto.h

--- Objeto.h
+++ Objeto.h
@@ -9,7 +9,8 @@ using namespace std;
 
 class Objeto {
 private:
-    string        nombre;
+    string nombre;
+    double calificacion;
     TipoCaracter  color;
     TipoEntero    posX;
     TipoEntero    posY;
@@ -17,6 +18,8 @@ public:
     Objeto();
     Objeto(const TipoString& nombre, TipoCaracter color,
            TipoEntero posX, TipoEntero posY);
+    Objeto(const TipoString& nombre, TipoCaracter color,
+           TipoEntero posX, TipoEntero posY, double calificacion);
     virtual ~Objeto();
     void setNombre(const TipoString& nombre);
     string     getNombre();
@@ -27,4 +30,13 @@ public:
     string mostrarPosicion();
 };
 
+class restaurante: public Objeto{
+    
+}
+class hoteles: public Objeto{
+    
+}
+class museo: public Objeto{
+    
+}
 #endif //AGREGACION_OBJETO_H
